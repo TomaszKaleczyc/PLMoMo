e7e38b6f051c8a749f42132ab95f2da6632dbd27
TomaszKaleczyc <tomasz.kaleczyc@gmail.com>
2022-03-02 12:15:02 +0100

    basic django db handler setup

--- backend/mortality_fact_updater/mortality_fact_updater.py
+++ backend/mortality_fact_updater/mortality_fact_updater.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from omegaconf import DictConfig
 
 from .mortality_actuals_extractor import MortalityActualsExtractor
+from .database_handler import DatabaseHandler
+from web.apps.mortality.django_database_handler import DjangoDatabaseHandler
 
 
 class MortalityFactUpdater:
@@ -16,10 +18,25 @@ class MortalityFactUpdater:
         self.cfg = cfg
         self.mortality_actuals_extractor = MortalityActualsExtractor(cfg=cfg)
         self.log = logging.getLogger(__name__)
+        self.database_handler: DatabaseHandler = self._get_database_handler()
+
+    @property
+    def db_type(self) -> str:
+        return self.cfg.db.type
+
+    def _get_database_handler(self) -> DatabaseHandler:
+        """
+        Returns the proper database handler for the project
+        """
+        self.log.info(f'Database connection type: {self.db_type}')
+        if self.db_type == 'django':
+            return DjangoDatabaseHandler()
+        raise NotImplementedError
 
     def update_db(self) -> None:
         """
         Updates the project database mortality facts
         """
         mortality_facts = self.mortality_actuals_extractor.extract_actuals()
+        self.database_handler.update_actuals(mortality_facts)
         
